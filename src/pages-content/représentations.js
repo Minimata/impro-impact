f2692bd046a95944002a1546bb9d36e91f8c6693
Damianvei <58422261+Damianvei@users.noreply.github.com>
2021-02-08 12:09:45 +0100

    Lieu --> en ligne

--- src/pages-content/représentations.js
+++ src/pages-content/représentations.js
@@ -1,7 +1,7 @@
 let content = {
     spectacle: "Helvetia2050",
     date: "4-5-6 mars 2021",
-    place: "Uni Mail, Bd du Pont d’Arve 40, 1205 Genève, Salle:Ms180",
+    place: "[En ligne]",
 }
 
 export default content
